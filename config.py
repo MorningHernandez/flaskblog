44cb00d00d2db93a7bddabab48f514c3b772f58b
A.M. Hernandez <mouseandweb@gmail.com>
2021-04-22 07:52:12 -0400

    added email support and pw reset

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['flask.debug@gmail.com']
 
     # pagination
     POSTS_PER_PAGE = 5
